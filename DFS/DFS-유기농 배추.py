2c968c1637769f788baf5408be1b4e5faea32e23
SeokLii <k1nder@naver.com>
2021-02-09 01:45:59 +0900

    알고리즘 풀이수정(2021-02-09)

--- DFS/DFS-유기농 배추.py
+++ DFS/DFS-유기농 배추.py
@@ -1,19 +1,20 @@
 import sys
-
 T = int(sys.stdin.readline())
 A_Rule = [1, -1, 0, 0]
 B_Rule = [0, 0, 1, -1]
 
 for _ in range(T):
     M, N, K = map(int, sys.stdin.readline().split())
-    MAP = [[0] * N for _ in range(M)]
+    MAP = [[0] * M for _ in range(N)]
     for _ in range(K):
         X, Y = map(int, sys.stdin.readline().split())
-        MAP[X][Y] = 1
+        MAP[Y][X] = 1
     Count = 0
-    for i in range(M):
-        for j in range(N):
-            if MAP[i][j] == 1:
+    for i in range(N):
+        for j in range(M):
+            if MAP[i][j] == 0:
+                continue
+            elif MAP[i][j] == 1:
                 Stack = []
                 Stack.append([i, j])
                 MAP[i][j] = 0
@@ -21,11 +22,16 @@ for _ in range(T):
                 while Stack:
                     A, B = Stack.pop()
                     for k in range(4):
-                        A += A_Rule[k]
-                        B += B_Rule[k]
-                        if 0 <= A < M and 0 <= B < N:
-                            if MAP[A][B] == 1:
-                                Stack.append([A, B])
-                                MAP[A][B] = 0
+                        A_sum = A + A_Rule[k]
+                        B_sum = B + B_Rule[k]
+                        # 이전코드
+                        # A += A_Rule
+                        # B += B_Rule
+                        # 이렇게 하게 되면 다음 for문에서 변한 A와 B에 덧셈을 진행하게 되서 틀리게됨
+                        if 0 <= A_sum < N and 0 <= B_sum < M:
+                            if MAP[A_sum][B_sum] == 1:
+                                Stack.append([A_sum, B_sum])
+                                MAP[A_sum][B_sum] = 0
+
     print(Count)
 
