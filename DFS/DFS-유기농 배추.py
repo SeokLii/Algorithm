d6e571a612645640a137e21ccbfa4bf35caf536d
SeokLii <k1nder@naver.com>
2021-02-08 22:26:06 +0900

    알고리즘 풀이수정(2021-02-08)

--- DFS/DFS-유기농 배추.py
+++ DFS/DFS-유기농 배추.py
@@ -1,7 +1,6 @@
 import sys
 
 T = int(sys.stdin.readline())
-Result = []
 A_Rule = [1, -1, 0, 0]
 B_Rule = [0, 0, 1, -1]
 
@@ -11,30 +10,22 @@ for _ in range(T):
     for _ in range(K):
         X, Y = map(int, sys.stdin.readline().split())
         MAP[X][Y] = 1
-
     Count = 0
-    for i in range(N):
-        for j in range(M):
+    for i in range(M):
+        for j in range(N):
             if MAP[i][j] == 1:
-                print(i, j)
                 Stack = []
                 Stack.append([i, j])
                 MAP[i][j] = 0
                 Count += 1
                 while Stack:
                     A, B = Stack.pop()
-                    print(A, B)
-                    for i in range(4):
-                        print(Stack)
-                        A += A_Rule[i]
-                        B += B_Rule[i]
+                    for k in range(4):
+                        A += A_Rule[k]
+                        B += B_Rule[k]
                         if 0 <= A < M and 0 <= B < N:
                             if MAP[A][B] == 1:
                                 Stack.append([A, B])
                                 MAP[A][B] = 0
+    print(Count)
 
-
-    Result.append(Count)
-
-for i in Result:
-    print(i)
