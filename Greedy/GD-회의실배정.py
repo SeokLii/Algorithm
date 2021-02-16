0f612d5ee2b70ae966f69c1e0c9f485a7d063505
SeokLii <k1nder@naver.com>
2021-02-16 22:09:06 +0900

    알고리즘 풀이수정(2021-02-16)

--- Greedy/GD-회의실배정.py
+++ Greedy/GD-회의실배정.py
@@ -17,3 +17,30 @@ for i in range(N):
 
 InputArr.sort(key=lambda x: (x[1], x[0]))
 print(solution(InputArr))
+
+
+# import sys
+# from operator import itemgetter
+#
+# N = int(sys.stdin.readline())
+# List = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+# List.sort(key=itemgetter(1))
+# print(List)
+
+n = int(input())
+s = []
+for i in range(n):
+    first, second = map(int, input().split())
+    s.append([first, second])
+s = sorted(s, key=lambda a: (a[1], a[0]))
+#s = sorted(s, key=lambda a: a[1])
+print(s)
+last = 0
+cnt = 0
+for i, j in s:
+    print(i, j)
+    if i >= last:
+        print(i, j)
+        cnt += 1
+        last = j
+print(cnt)
