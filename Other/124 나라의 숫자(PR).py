a4b1914913e91f6ceff132fcad1adfeb1b7599d2
SeokLii <k1nder@naver.com>
2023-01-18 23:45:09 +0900

    알고리즘 풀이(2023-01-18)

--- Other/124 나라의 숫자(PR).py
+++ Other/124 나라의 숫자(PR).py
@@ -1,3 +1,76 @@
+def solution(n):
+    answer = ''
+    while n:
+        if n % 3:
+            answer += str(n % 3)
+            n //= 3
+        else:
+            answer += "4"
+            n = n//3 - 1
+    return answer[::-1]
+
+
+
+
+
+from collections import deque
+
+def solution(n):
+    answer = ''
+    country = deque()
+    while n:
+        if n % 3 == 0:
+            if country[-1] == '1':
+                country.pop()
+                country.append(4)
+            else:
+                country[-1] -= 1
+                country.append(4)
+
+        country.append(str(n % 3))
+        n //= 3
+
+    answer = list(answer[::-1])
+    for i in range(1, len(answer)):
+        if answer[i] == '0':
+            answer[i - 1] = str(int(answer[i - 1]) - 1)
+            answer[i] = '4'
+
+    return str(int(''.join(answer)))
+
+
+1
+1
+2
+2
+3
+10
+4
+4
+11
+5
+12
+6
+20
+14
+7
+21
+8
+22
+9
+100
+24
+10
+101
+41
+11
+110
+
+
+
+
+
+
 # K진법 구하고 0이 나오는 숫자에 4를 대신 넣어주면 됨
 # 결국은 3진법을 구하는 문제
 def solution(n):
