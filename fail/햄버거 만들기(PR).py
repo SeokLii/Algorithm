29148f374294c3ba0249350cf16a70691f3b0fde
SeokLii <k1nder@naver.com>
2023-02-07 00:52:06 +0900

    알고리즘 풀이(2023-02-07)

--- fail/햄버거 만들기(PR).py
+++ fail/햄버거 만들기(PR).py
@@ -1,5 +1,23 @@
-from collections import deque
+def solution(ingredient):
+    answer = 0
+    recipe = [1, 2, 3, 1]
+    deq = []
+    for i in ingredient:
+        deq.append(i)
+        while len(deq) >= 4:
+            if deq[-4:] != recipe:
+                break
+            else:
+                answer += 1
+                for _ in range(4):
+                    deq.pop()
 
+    return answer
+
+#잘못된 풀이
+#시간을 단축하기 위해 deque를 썻지만, sliceing 하기위해 deque를 list로 변경해주는 과정이 O(n) 만큼 더 많은 시간을 사용하게됨
+
+from collections import deque
 
 def solution(ingredient):
     answer = 0
