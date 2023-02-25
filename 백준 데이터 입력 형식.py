3e9d90931967c81313aae4303bb7a896da07309c
SeokLii <k1nder@naver.com>
2023-02-25 20:40:30 +0900

    알고리즘 풀이 (2023-02-25)

---
+++ 백준 데이터 입력 형식.py
@@ -0,0 +1,100 @@
+# 하나의 변수 입력
+# str 형
+x = input() # 기본적으로 str 형식으로 입력됨
+# int 형
+y = int(input())
+
+print(x, type(x), y, type(y)) # 1 <class 'str'> 2 <class 'int'>
+
+# 여러 개의 변수 입력
+# str 형
+N, M = input().split() # str 형식으로 입력되어서 int 로 입력 받기 위해서는 map 사용해야함
+# int 형
+L, O = map(int, input().split()) # int(input().split())은 여러 개를 입력 받을 수 없음
+
+print(N, M, L, O) # a b 3 4
+
+# 리스트 형 입력
+arr1 = map(int, input().split()) # 리스트 형 변환 없이 하나의 변수에 2가지 이상의 값을 저장하게 되면 map 타입으로 저장됨
+
+# 하나의 변수에 여러 값을 넣으려면 list 형식으로 넣어서 사용해야 함
+# str 형
+arr2 = list(input().split())
+# int 형
+arr3 = list(map(int, input().split()))
+arr4 = [list(map(int, input().split())) for _ in range(L)] # 이중 리스트로 받을 수 있음
+
+print(arr1) # <map object at 0x000001FF1FD6B940>
+print(arr2) # ['a', 'b']
+print(arr3) # [7, 8]
+print(arr4) # [[9, 10], [11, 12], [13, 14]]
+
+# 빠른 입력
+import sys
+# 하나의 변수 입력
+# str 형
+a = sys.stdin.readline() # 기본적으로 str 형식으로 입력됨
+b = sys.stdin.readline().rstrip() # 개행 문자(줄바꿈)가 같이 입력되기 때문에 rstrip()으로 없애줘야함
+
+print(a, b, type(b)) #a\n b <class 'str'>
+# int 형
+c = int(sys.stdin.readline()) # 숫자는 개행 문자 없이 입력되어 rstrip()이 필요 없음
+d = int(sys.stdin.readline().rstrip())
+
+print(c, d, type(d)) # 1 2 <class 'int'>
+# 여러 개의 변수 입력
+# str 형
+e, f = sys.stdin.readline().split() # split()을 사용하면 개행 문자가 입력되지 않음
+g, h = sys.stdin.readline().split(' ') # split(' ') 를 사용하면 띄어쓰기 빼고 개행 문자는 받아와져서 개행 문자는 입력됨
+# int 형
+i, j = map(int, sys.stdin.readline().split())
+
+print(e, f) # c d
+print(g, h) # e f\n
+print(i, j) # 3 4
+
+# 리스트 형 입력
+srr1 = map(int, sys.stdin.readline())
+
+# 하나의 변수에 여러 값을 넣으려면 list 형식으로 넣어서 사용해야 함
+# str 형
+srr2 = list(sys.stdin.readline().split()) # 개행 문자 X, but split(' ') 사용하면 개행 문자 들어옴
+# int 형
+srr3 = list(map(int, sys.stdin.readline().split()))
+srr4 = [list(map(int, sys.stdin.readline().split())) for _ in range(2)] # 이중 리스트로 받을 수 있음
+print(srr1) # <map object at 0x00000190A406FFD0>
+print(srr2) # ['a', 'b']
+print(srr3) # [3, 4]
+print(srr4) # [[5, 6], [7, 8]]
+
+# 입력 데이터
+"""
+1
+2
+a b
+3 4
+5 6
+a b
+7 8
+9 10
+11 12
+13 14
+1
+2
+3 4
+5 6
+
+a
+b
+1
+2
+c d
+e f
+3 4
+1 2
+a b
+3 4
+5 6
+7 8
+
+"""
