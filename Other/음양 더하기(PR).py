e636528da013768177906562fcdebd922923d022
SeokLii <k1nder@naver.com>
2023-03-14 23:30:42 +0900

    알고리즘 풀이(2023-03-14)

--- Other/음양 더하기(PR).py
+++ Other/음양 더하기(PR).py
@@ -1,3 +1,8 @@
 def solution(absolutes, signs):
-    answer = 123456789
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
     return answer
