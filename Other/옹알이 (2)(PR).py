d4836c5e04d071a17701c596e45a5571b5fa1c2a
SeokLii <k1nder@naver.com>
2023-04-11 23:23:12 +0900

    알고리즘 풀이(2023-04-11)

--- Other/옹알이 (2)(PR).py
+++ Other/옹알이 (2)(PR).py
@@ -1,5 +1,21 @@
 def solution(babbling):
     answer = 0
+
+    for i in babbling:
+        for j in ['aya', 'ye', 'woo', 'ma']:
+            if i.count(j * 2) >= 1:
+                break
+            i = i.replace(j, '*')
+
+        if len(i) == i.count('*'):
+            answer += 1
+
+    return answer
+
+# 잘못된 풀이
+# 연속된 옹알이를 체크한것이 아니여서 잘못된 풀이
+def solution(babbling):
+    answer = 0
     for i in babbling:
         for j in ['aya', 'ye', 'woo', 'ma']:
             if i.count(j) >= 2:
