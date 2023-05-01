cd4460a4bdcf0ca088f8fed5d065f3b999b5eab1
SeokLii <k1nder@naver.com>
2023-05-02 00:38:10 +0900

    알고리즘 풀이(2023-05-02)

--- Other/합성수 찾기(PR).py
+++ Other/합성수 찾기(PR).py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 0
 
-    for i in range(1, n):
-        for j in range(2, int(i ** (0.5))):
-            if j % i == 0:
+    for i in range(4, n + 1):
+        for j in range(2, int(i ** (0.5)) + 1):
+            if i % j == 0:
                 answer += 1
                 break
 
